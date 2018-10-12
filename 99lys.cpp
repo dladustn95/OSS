2a75f59430562e2ce342e835359d58dd97e02b81
YeonSooLim <2014104139@khu.ac.kr>
2018-10-12 10:05:32 +0900

    99dan push LYS

---
+++ 99lys.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+use namespace std;
+
+int main(){
+	cout << "±¸±¸´Ü" << endl;
+
+	for(int i=1;i<9;i++)
+	{	
+		for(int j=1;j<10;j++)
+		{
+			cout << i << " X " << j << " = " << i*j << endl;
+		}
+	}
+
+	return 0;
+}		
